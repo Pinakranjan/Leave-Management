5fca73d7f53f5e6661b27cb4a82ac3c0c8dfc3a1
Pinak Sahoo <pinak_hits@hotmail.com>
2020-03-15 11:31:23 +0530

    Setup User Auth, Default Roles and Admin and Modified Registration Process

--- leave-management/SeedData.cs
+++ leave-management/SeedData.cs
@@ -17,11 +17,11 @@ namespace leave_management
 
         private static void SeedUsers(UserManager<IdentityUser> userManager)
         {
-            if(userManager.FindByNameAsync("admin").Result == null)
+            if(userManager.FindByNameAsync("admin@localhost.com").Result == null)
             {
                 var user = new Employee
                 {
-                    UserName = "admin",
+                    UserName = "admin@localhost.com",
                     Email = "admin@localhost.com",
                     Firstname="admin",
                     Lastname="Test",
